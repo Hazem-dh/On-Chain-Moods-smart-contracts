25fe655ac1f603d8339ff7529271ab986da499d4
hazem-dh <58445913+Hazem-dh@users.noreply.github.com>
2026-02-12 00:09:37 +0100

    Fix typo i

--- test/MoodNFT.sol
+++ test/MoodNFT.sol
@@ -8,7 +8,7 @@ contract MoodNFTTest is Test {
     MoodNFT public moodNFT;
 
     function setUp() public {
-        string memory angry = vm.readFile("./images/anfry.svg");
+        string memory angry = vm.readFile("./images/angry.svg");
         string memory happy = vm.readFile("./images/happy.svg");
         string memory neutral = vm.readFile("./images/neutral.svg");
         string memory sleepy = vm.readFile("./images/sleepy.svg");
