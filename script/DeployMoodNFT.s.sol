ea508683c3db444aa78f0a4f3472e448e5cad937
hazem-dh <58445913+Hazem-dh@users.noreply.github.com>
2026-02-26 00:18:52 +0100

    prepared deployments , mock and test contracts

---
+++ script/DeployMoodNFT.s.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script} from "forge-std/Script.sol";
+import {MoodNFT} from "../src/MoodNFT.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+contract DeployMoodNFTScript is Script {
+    MoodNFT public moodNft;
+
+    function setUp() public {}
+
+    function run() public returns (address) {
+        string memory happy = vm.readFile("./assets/happy.svg");
+        string memory sad = vm.readFile("./assets/sad.svg");
+        string memory satisfied = vm.readFile("./assets/satisfied.svg");
+        HelperConfig helperConfig = new HelperConfig(); // This comes with our mocks!
+
+        (address wethUsdPriceFeed, uint256 deployerKey) = helperConfig.activeNetworkConfig();
+        vm.startBroadcast(deployerKey);
+
+        moodNft = new MoodNFT(sad, satisfied, happy, wethUsdPriceFeed);
+        vm.stopBroadcast();
+
+        return address(moodNft);
+    }
+}
---
+++ test/unit/MoodNFTTEST.t.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test} from "forge-std/Test.sol";
+import {MoodNFT} from "../../src/MoodNFT.sol";
+import {DeployMoodNFTScript} from "../../script/DeployMoodNFT.s.sol";
+
+contract MoodNFTTest is Test {
+    DeployMoodNFTScript public deployer;
+    MoodNFT public moodNft;
+
+    function setUp() public {
+        //string memory happy = vm.readFile("./assets/happy.svg");
+        //string memory sad = vm.readFile("./assets/sad.svg");
+        //string memory satisfied = vm.readFile("./assets/satisfied.svg");
+
+        deployer = new DeployMoodNFTScript();
+        moodNft = MoodNFT(deployer.run());
+    }
+
+    function test_Increment() public {
+        MoodNFT(moodNft).mint(1000);
+        assertEq(MoodNFT(moodNft).balanceOf(msg.sender), 1);
+    }
+}
